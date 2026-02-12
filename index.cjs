8c7c61b772c417751262195a561bbdb3cf36e089
Kolczatekk <michalmagicc@gmail.com>
2026-02-12 19:37:19 +0100

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -2990,7 +2990,9 @@ async function handleModalSubmit(interaction) {
       .setColor(COLOR_BLUE)
       .setTitle(embedTitle)
       .setDescription(
-        `## \`🛒 New Shop × ${ticketTypeLabel}\`\n` +
+        "```\n" +
+        `## 🛒 New Shop × ${ticketTypeLabel}\n` +
+        "```\n" +
         `### ・ 👤 × Informacje o kliencie:\n` +
         `> <a:arrowwhite:1469100658606211233> × **Ping:** <@${user.id}>\n` +
         `> <a:arrowwhite:1469100658606211233> × **Nick:** \`${interaction.member?.displayName || user.globalName || user.username}\`\n` +
@@ -6599,9 +6601,9 @@ async function handleModalSubmit(interaction) {
         const embed = new EmbedBuilder()
           .setColor(COLOR_BLUE)
           .setDescription(
-                     ````\n" +
-            `🛒 New Shop × ${ticketTypeLabel}\n` +
-                    ````\n" +
+            "```\n" +
+            `## 🛒 New Shop × ${ticketTypeLabel}\n` +
+            "```\n" +
             `### ・ \`👤\` × Informacje o kliencie:\n` +
             `> <a:arrowwhite:1469100658606211233> × **Ping:** <@${user.id}>\n` +
             `> <a:arrowwhite:1469100658606211233> × **Nick:** \`${interaction.member?.displayName || user.globalName || user.username}\`\n` +
